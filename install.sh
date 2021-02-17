c7a2ec7669a52cc92e1f2ff294a5570ed7ffe002
Dave Shifflett <dave@corpulent.coffee>
2021-02-16 19:10:01 -0600

    Handle already-existing files
    Do this by updating the file in the repository with what's on disk and
    then creating the symlink anyway.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ shopt -s inherit_errexit
 cd "$(dirname "${BASH_SOURCE[0]}")/home"
 
 skipCount=0
+copyCount=0
 
 while IFS='' read -rd '' srcPath; do
   srcDir="$(dirname "$srcPath")"
@@ -24,17 +25,29 @@ while IFS='' read -rd '' srcPath; do
       ((++skipCount))
       echo "$srcPath: $dstPath already linked to $dstLink instead of $srcReal"
     fi
+  elif [ -f "$dstPath" ]; then
+    ((++copyCount))
+    echo "$srcPath: $dstPath already exists; use version control to reconcile"
+    cp --verbose "$dstPath" "$srcPath"
+    ln --force --relative --symbolic --verbose "$srcPath" "$dstPath"
   else
     mkdir --parents --verbose "$dstDir"
     ln --relative --symbolic --verbose "$srcPath" "$dstPath"
   fi
 done < <(find . -type f -print0)
 
-if [ "$skipCount" -gt 0 ]; then
+if [[ "$skipCount" -gt 0 || "$copyCount" -gt 0 ]]; then
   if [ "$skipCount" -eq 1 ]; then
     echo 'One file could not be installed.' >&2
-  else
+  elif [ "$skipCount" -gt 0 ]; then
     echo "$skipCount files could not be installed." >&2
   fi
+
+  if [ "$copyCount" -eq 1 ]; then
+    echo 'One file already exists and needs to be reconciled.' >&2
+  elif [ "$copyCount" -gt 0 ]; then
+    echo "$copyCount files already exist and need to be reconciled." >&2
+  fi
+
   exit 1
 fi
