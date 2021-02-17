c80531c1359ca2f63088163e307341059f3814ea
Dave Shifflett <dave@corpulent.coffee>
2021-02-16 18:13:44 -0600

    Implement basic installation functionality
    This script handles one special case at present: it can be run multiple
    times with already-existing symbolic links and as long as the symlinks
    point to the same place, they will be ignored.

--- install.sh
+++ install.sh
@@ -1,12 +1,40 @@
 #!/usr/bin/env bash
 
-function main() {
-  echo "Hello world!"
-}
+set -euETo pipefail
+shopt -s inherit_errexit
+cd "$(dirname "${BASH_SOURCE[0]}")/home"
 
-if [ "$0" == "${BASH_SOURCE[0]}" ]; then # we weren't `source`d (for debugging)
-  set -euETo pipefail
-  shopt -s inherit_errexit
+skipCount=0
 
-  main
+while IFS='' read -rd '' srcPath; do
+  srcDir="$(dirname "$srcPath")"
+  dstDir="$HOME/$srcDir"
+  dstPath="$HOME/$srcPath"
+
+  # Special case: install Python and shell script bin files without suffix.
+  if [[ "$srcDir" == './bin' && "$srcPath" =~ \.(py|sh)$ ]]; then
+    dstPath="${dstPath%.*}"
+  fi
+
+  if [ -L "$dstPath" ]; then
+    srcReal="$(realpath "$srcPath")"
+    dstLink="$(readlink --canonicalize "$dstPath")"
+
+    if [ "$srcReal" != "$dstLink" ]; then
+      ((++skipCount))
+      echo "$srcPath: $dstPath already linked to $dstLink instead of $srcReal"
+    fi
+  else
+    mkdir --parents --verbose "$dstDir"
+    ln --relative --symbolic --verbose "$srcPath" "$dstPath"
+  fi
+done < <(find . -type f -print0)
+
+if [ "$skipCount" -gt 0 ]; then
+  if [ "$skipCount" -eq 1 ]; then
+    echo 'One file could not be installed.' >&2
+  else
+    echo "$skipCount files could not be installed." >&2
+  fi
+  exit 1
 fi
