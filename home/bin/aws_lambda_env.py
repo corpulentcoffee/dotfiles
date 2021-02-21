16a90acb620ee52862fb14430fd416904ef999f3
Dave Shifflett <dave@corpulent.coffee>
2021-02-21 11:45:07 -0600

    Separate auth variables from other local ones
    This allows separated control of whether to inherit which ones.

--- home/bin/aws_lambda_env.py
+++ home/bin/aws_lambda_env.py
@@ -6,7 +6,6 @@ from boto3 import Session
 
 
 def main():
-    from os import environ
     from shlex import quote
     from subprocess import run
 
@@ -21,7 +20,12 @@ def main():
         print(*map(quote, args.command), file=stderr)
         print(file=stderr)
 
-        run(args.command, env=vars if args.no_inherit else {**environ, **vars})
+        env = {
+            **({} if args.no_local_env else get_local_vars()),
+            **({} if args.no_auth_env else get_auth_vars(session)),
+            **vars,
+        }
+        run(args.command, env=env)
 
     else:  # ".env file"-compatible output to stdout
         print(*vars_quoted, sep="\n")
@@ -37,8 +41,9 @@ def get_parser():
         """,
         epilog="""
             Note that any command run will be using the AWS credentials of the
-            user invoking the script, not the permissions of the role used by a
-            real invocation of the Lambda function.
+            user invoking the script (or no credentials at all if --no-auth-env
+            is used); the command will not run with the permissions of the role
+            used by a real invocation of the Lambda function.
         """,
     )
     parser.add_argument(
@@ -58,11 +63,17 @@ def get_parser():
         required=True,
     )
     parser.add_argument(
-        "--no-inherit",
+        "--no-local-env",
         help="""
-            when running a command, do not inherit user's local environment
-            variables
+            when running a command, do not inherit caller's local environment
+            variables that aren't related to AWS credentials; see also
+            --no-auth-env
         """,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--no-auth-env",
+        help="when running a command, do not inherit caller's AWS credentials",
         action="store_true",
     )
     parser.add_argument(
@@ -81,18 +92,6 @@ def get_lambda_vars(session: Session, function_name: str):
     awslambda = session.client("lambda")
     response = awslambda.get_function(FunctionName=function_name)
     config = response["Configuration"]
-
-    # In a real Lambda invocation, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,
-    # and AWS_SESSION_TOKEN would be set rather than using the credentials of
-    # the user invoking this script (e.g. AWS_PROFILE).
-    #
-    # Ideally, this script would assume the role of the function and set said
-    # variables (e.g. to test IAM policies), but that is usually not possible.
-    # Usually, the trusted entity for a function role is lambda.amazonaws.com
-    # (and maybe edgelambda.amazonaws.com), and that does *not* allow IAM users
-    # of an AWS account to assume the role.
-    profile_name = session.profile_name
-    auth_vars = {"AWS_PROFILE": profile_name} if profile_name else {}
 
     # This is a partial list; for all built-in environment variables, see:
     # <https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html>
@@ -115,8 +114,51 @@ def get_lambda_vars(session: Session, function_name: str):
             file=stderr,
         )
 
-    merged_vars = {**auth_vars, **builtin_vars, **function_vars}
+    merged_vars = {**builtin_vars, **function_vars}
     return {str(key): str(value) for key, value in merged_vars.items()}
+
+
+def get_local_vars():
+    from os import environ
+
+    return {
+        key: value
+        for key, value in environ.items()
+        if key
+        not in [
+            "AWS_PROFILE",
+            "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY",
+            "AWS_SESSION_TOKEN",
+        ]
+    }
+
+
+def get_auth_vars(session: Session):
+    """
+    In real invocations, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and
+    AWS_SESSION_TOKEN would be set rather than using the credentials of
+    the user invoking this script (i.e. usually AWS_PROFILE).
+
+    Ideally, this script would assume the role of the function and set
+    said variables (e.g. to test IAM policies), but that is usually not
+    possible. Usually, the trusted entity for a function role is
+    lambda.amazonaws.com (and maybe edgelambda.amazonaws.com), and that
+    does *not* allow IAM users of an AWS account to assume the role.
+    """
+
+    from os import getenv
+
+    return {
+        key: str(value)
+        for key, value in {
+            "AWS_PROFILE": session.profile_name,
+            "AWS_ACCESS_KEY_ID": getenv("AWS_ACCESS_KEY_ID"),
+            "AWS_SECRET_ACCESS_KEY": getenv("AWS_SECRET_ACCESS_KEY"),
+            "AWS_SESSION_TOKEN": getenv("AWS_SESSION_TOKEN"),
+        }.items()
+        if value and value != "default"
+    }
 
 
 if __name__ == "__main__":
