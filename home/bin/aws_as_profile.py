d8fc870d524a5eb521e04b3ac7b26a593055d607
Dave Shifflett <dave@corpulent.coffee>
2021-03-04 20:54:19 -0600

    Implement command formulation for aws-as-profile
    This sets up how the aws-as-role command will be called.

--- home/bin/aws_as_profile.py
+++ home/bin/aws_as_profile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Tuple
+from typing import List, Optional, Tuple
 
 AS_ROLE = "aws-as-role"
 PROFILE_PATH = "~/.aws/credentials"
@@ -10,9 +10,15 @@ def main():
     args = get_parser().parse_args()
     profile_config = get_profile_config()
     destination_config = profile_config[args.profile]
-    destination_role_arn = destination_config["role_arn"]
-    source_profile_name = destination_config["source_profile"]
-    print(destination_role_arn, "via", source_profile_name)
+    as_role_command = get_as_role_command(
+        source_profile=destination_config["source_profile"],
+        destination_role_arn=destination_config["role_arn"],
+        session_name=args.session_name,
+        serial_number=destination_config.get("mfa_serial"),
+        duration_seconds=args.duration_seconds,
+        command=args.command,
+    )
+    print(as_role_command)
 
 
 def get_parser():
@@ -105,6 +111,30 @@ def get_profile_config():
     profile_config = ConfigParser()
     profile_config.read(profile_path)
     return profile_config
+
+
+def get_as_role_command(
+    source_profile: str,
+    destination_role_arn: str,
+    session_name: Optional[str],
+    serial_number: Optional[str],
+    duration_seconds: Optional[str],
+    command: List[str],
+):
+    as_role_command = ["aws-as-role"]
+    as_role_command.extend(["--profile", source_profile])
+    as_role_command.extend(["--role-arn", destination_role_arn])
+
+    if session_name:
+        as_role_command.extend(["--session-name", session_name])
+    if serial_number:
+        as_role_command.extend(["--serial-number", serial_number])
+    if duration_seconds:
+        as_role_command.extend(["--duration-seconds", duration_seconds])
+    if command:
+        as_role_command.extend(["--", *command])
+
+    return as_role_command
 
 
 if __name__ == "__main__":
