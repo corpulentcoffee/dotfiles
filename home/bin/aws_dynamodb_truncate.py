5dcef6e81fa789a4c78991217e1fe4834d6d99a2
Dave Shifflett <dave@corpulent.coffee>
2021-03-05 20:49:29 -0600

    Item fetching and typing for aws-dynamodb-truncate
    Use a DynamoValue and DynamoItem type to represent these.

--- home/bin/aws_dynamodb_truncate.py
+++ home/bin/aws_dynamodb_truncate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from typing import List, Tuple
+from typing import Dict, List, Tuple, cast
+
+DynamoValue = Dict[str, str]  # e.g. {"N": "12345"}
+DynamoItem = Dict[str, DynamoValue]  # e.g. {"id": {"N": "12345"}}
 
 
 def main():
@@ -8,7 +11,8 @@ def main():
     client = get_dynamodb_client(args.profile, args.region)
     table_name = args.table_name
     item_count, keys = get_table_info(client, table_name)
-    print(item_count, keys)
+    items = get_items(client, table_name, keys)
+    print(item_count, next(items))
 
 
 def get_parser():
@@ -55,6 +59,24 @@ def get_table_info(client, table_name: str) -> Tuple[int, List[str]]:
     item_count = table["ItemCount"]
     keys = [definition["AttributeName"] for definition in table["KeySchema"]]
     return item_count, keys
+
+
+def get_items(client, table_name: str, attrs: List[str]):
+    enumerated = list(enumerate(attrs))
+    params = dict(
+        TableName=table_name,
+        ProjectionExpression=", ".join(f"#attr{i}" for i, _ in enumerated),
+        ExpressionAttributeNames={f"#attr{i}": name for i, name in enumerated},
+    )
+
+    while params:
+        result = client.scan(**params)
+        yield from cast(List[DynamoItem], result["Items"])
+
+        if result.get("LastEvaluatedKey"):
+            params["ExclusiveStartKey"] = result["LastEvaluatedKey"]
+        else:
+            params = None
 
 
 if __name__ == "__main__":
