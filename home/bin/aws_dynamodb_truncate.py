3047fd2b97aa3335b1c7e792b021455ed6d1f2ff
Dave Shifflett <dave@corpulent.coffee>
2021-03-05 20:10:53 -0600

    Add argparse parser for aws-dynamodb-truncate
    The only thing required here is the table name; the other two could come
    from environment variables if set.

---
+++ home/bin/aws_dynamodb_truncate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+def main():
+    args = get_parser().parse_args()
+    print(args)
+
+
+def get_parser():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description="Delete all items in a DynamoDB table.",
+        epilog="""
+            If your table contains many items, consider deleting the table and
+            creating it again as a cheaper/faster alternative.
+        """,
+    )
+    parser.add_argument(
+        "--profile",
+        help="""
+            use named AWS profile (e.g. "development") or omit to use
+            environment variables
+        """,
+    )
+    parser.add_argument(
+        "--region",
+        help="region where DynamoDB table is provisioned",
+    )
+    parser.add_argument(
+        "--table-name",
+        help="name of table whose items will be deleted",
+        metavar="TABLE",
+        required=True,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    main()
