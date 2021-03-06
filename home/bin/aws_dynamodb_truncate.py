36f3e155573d20b8e8116c36df215c4cd50121fb
Dave Shifflett <dave@corpulent.coffee>
2021-03-06 10:46:25 -0600

    aws-dynamodb-truncate: use Table resource thru end
    This allows cleaning up a ton of code.

--- home/bin/aws_dynamodb_truncate.py
+++ home/bin/aws_dynamodb_truncate.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 
-from typing import Dict, Iterable, List, cast
-
-from boto3 import Session
-
-DynamoValue = Dict[str, str]  # e.g. {"N": "12345"}
-DynamoItem = Dict[str, DynamoValue]  # e.g. {"id": {"N": "12345"}}
+from typing import List, cast
 
 
 def main() -> int:
     args = get_parser().parse_args()
-    session = Session(profile_name=args.profile, region_name=args.region)
+    table = get_table(args.profile, args.region, args.table_name)
+    pages = get_item_pages(table)
+    first_page = next(pages)
 
-    # see https://github.com/boto/boto3/issues/2039
-    client = session.client("dynamodb")
-    table = session.resource("dynamodb").Table(args.table_name)
-
-    batches = in_batches(get_items(client, table))
-    try:
-        first_batch = next(batches)
-    except StopIteration:
+    if not first_page:
         print(f"{table.name} is already empty.")
         return 0
-
-    if get_confirmation(table.name, table.item_count, first_batch) is not True:
+    elif get_confirmation(table, first_page) is not True:
         return 1
 
-    delete_items(client, table.name, first_batch)
-    for successive_batch in batches:
-        delete_items(client, table.name, successive_batch)
-
+    with table.batch_writer() as batch_writer:  # also handles UnprocessedItems
+        delete_items(batch_writer, first_page)
+        for successive_page in pages:
+            delete_items(batch_writer, successive_page)
     return 0
 
 
@@ -64,7 +53,14 @@ def get_parser():
     return parser
 
 
-def get_items(client, table):
+def get_table(profile: str, region: str, table_name: str):
+    from boto3 import Session
+
+    session = Session(profile_name=profile, region_name=region)
+    return session.resource("dynamodb").Table(table_name)
+
+
+def get_item_pages(table):
     keys = [definition["AttributeName"] for definition in table.key_schema]
     enumerated = list(enumerate(keys))
     params = dict(
@@ -74,48 +70,34 @@ def get_items(client, table):
     )
 
     while params:
-        result = client.scan(**params)
-        yield from cast(List[DynamoItem], result["Items"])
+        result = table.scan(**params)
+        yield cast(List[dict], result["Items"])
 
+        # one can either have the paginator from the DynamoDB client or data
+        # marshalling from the Table resource, and implementing the former is
+        # easier than the latter; see https://github.com/boto/boto3/issues/2039
         if result.get("LastEvaluatedKey"):
             params["ExclusiveStartKey"] = result["LastEvaluatedKey"]
         else:
             params = None
 
 
-def in_batches(sequence: Iterable[DynamoItem], count: int = 25):
-    accumulated: List[DynamoItem] = []
-
-    for item in sequence:
-        accumulated.append(item)
-        if len(accumulated) == count:
-            yield accumulated
-            accumulated = []
-
-    if accumulated:
-        yield accumulated
-
-
-def get_confirmation(
-    table_name: str,
-    item_count: int,
-    sample: List[DynamoItem],
-) -> bool:
+def get_confirmation(table, sample: List[dict]) -> bool:
     from textwrap import dedent
 
     estimate = (
-        f"About {item_count} items are estimated to be in this table."
-        if item_count > 0
+        f"About {table.item_count} items are estimated to be in this table."
+        if table.item_count > 0
         else "An estimate for the item count of this table is not available."
     )
-    if item_count >= 100_000:
+    if table.item_count >= 100_000:
         estimate += (
             " For a table of this size, consider deleting and recreating it "
             "as a faster and cheaper alternative."
         )
 
     prompt = f"""
-        All items from {table_name} will be deleted.
+        All items from {table.name} will be deleted.
         {estimate}
 
         This is the first batch of items that would be deleted:
@@ -127,44 +109,14 @@ def get_confirmation(
     return input(f"{dedent(prompt).strip()} ").lower().startswith("y")
 
 
-def delete_items(client, table_name: str, items: List[DynamoItem]):
+def delete_items(batch_writer, items: List[dict]):
     print("deleting:", get_formatted_items(items))
-
-    request_items = {
-        table_name: [{"DeleteRequest": {"Key": item}} for item in items]
-    }
-    backoff_time = 1
-
-    while request_items:
-        response = client.batch_write_item(RequestItems=request_items)
-
-        if response.get("UnprocessedItems"):
-            from time import sleep
-
-            request_items = cast(dict, response["UnprocessedItems"])
-            print(
-                f"Retrying {len(request_items)}",
-                "item" if len(request_items) == 1 else "items",
-                f"after a {backoff_time}-second delay",
-            )
-
-            sleep(backoff_time)
-            backoff_time *= 2
-
-        else:
-            request_items = None
+    for item in items:
+        batch_writer.delete_item(item)
 
 
-def get_formatted_items(items: List[DynamoItem]) -> str:
-    return ", ".join(get_formatted_item(item) for item in items)
-
-
-def get_formatted_item(item: DynamoItem) -> str:
-    return " w/ ".join(
-        f'{key}="{value_str}"'
-        for key, value in item.items()
-        for value_str in value.values()
-    )
+def get_formatted_items(items: List[dict]) -> str:
+    return ", ".join(repr(item) for item in items)
 
 
 if __name__ == "__main__":
