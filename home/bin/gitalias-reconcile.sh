049901e445285d19bee4339648685f2c9d818854
Dave Shifflett <dave@corpulent.coffee>
2021-03-13 20:53:44 -0600

    Don't always add untracked file in `git reconcile`
    This only makes sense with certain flags; sniff for them.

--- home/bin/gitalias-reconcile.sh
+++ home/bin/gitalias-reconcile.sh
@@ -23,13 +23,17 @@ git symbolic-ref HEAD >/dev/null # we want to error out if off-branch
 repoStatus="$(git status --porcelain)"
 
 if [ "${#repoStatus}" -ne 0 ]; then # dirty working directory
-  mapfile -t newFiles < <(echo "$repoStatus" | grep '^?? ' | cut -d' ' -f2-)
-  for newFile in "${newFiles[@]}"; do
-    read -rp "Add $newFile? "
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      git stage --intent-to-add --verbose -- "$newFile"
-    fi
-  done
+  # Hacky (should parse arguments for real), but, if an interactive commit or
+  # adding all files, then there is probably interest in adding untracked files.
+  if [[ "$*" =~ (^| )(-[a-zA-Z]*[ap]|--(all|interactive|patch)) ]]; then
+    mapfile -t newFiles < <(echo "$repoStatus" | grep '^?? ' | cut -d' ' -f2-)
+    for newFile in "${newFiles[@]}"; do
+      read -rp "Add $newFile? "
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        git stage --intent-to-add --verbose -- "$newFile"
+      fi
+    done
+  fi
 
   # Note that `git commit` returns a non-zero status if nothing is commited.
   echo
