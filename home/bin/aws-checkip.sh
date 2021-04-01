5f755a99da31cddfb4969e6d8b58089650d6df4e
Dave Shifflett <dave@corpulent.coffee>
2021-04-01 07:49:29 -0500

    Add aws-checkip bin entry
    Via simple script to wrap curl call.

---
+++ home/bin/aws-checkip.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Output current public IP based on the `checkip` endpoint at AWS.
+
+set -euETo pipefail
+shopt -s inherit_errexit
+
+readonly endpoint='https://checkip.amazonaws.com/'
+
+if ! curl --fail "$endpoint"; then
+  echo "Unable to query $endpoint" >&2
+  exit 1
+fi
