1591038efa5df400b209a60514d23c69873917dd
Dave Shifflett <dave@corpulent.coffee>
2024-03-17 14:34:57 -0500

    split update-pip-packages off update-pip-user-packages

---
+++ home/bin/update-pip-packages.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# Do an upgrade of all pip packages using the current environment's `pip3`,
+# which may be the system `pip3`, or may be e.g. some `pyenv`-supplied one.
+
+set -euETo pipefail
+shopt -s inherit_errexit
+
+isUser=false
+if [[ $# -gt 0 ]]; then
+  if [[ $# -eq 1 && $1 == --user ]]; then
+    isUser=true
+  else
+    echo "usage: $0 [--user]" >&2
+    exit 1
+  fi
+fi
+
+pip=pip3
+pip=$(command -v "$pip")
+
+# TODO add more checks here? e.g. `|| command -v pyenv` ? be more paranoid? check PATH?
+if [[ -v PYENV_ROOT && -n $PYENV_ROOT && $pip == "$PYENV_ROOT/shims/"* ]]; then
+  installVersion=$(pyenv version-name)
+  if [[ $installVersion =~ ^3\.[0-9]+\.[0-9]+$ ]]; then
+    echo "!! $pip shim to '$installVersion'" >&2
+  else
+    echo "$0 refusing to use $pip shim to '$installVersion'" >&2
+    exit 1
+  fi
+elif ! [[ $isUser == true ]]; then
+  echo "$0 refusing to use system $pip w/o --user" >&2
+  exit 1
+fi
+
+declare -A outdatedVersions=()
+declare -A latestVersions=()
+
+while IFS=' ' read -ra line; do
+  if [[ -n "${line[*]}" ]]; then
+    test ${#line[@]} == 3
+    outdatedVersions[${line[0]}]=${line[1]}
+    latestVersions[${line[0]}]=${line[2]}
+  fi
+done <<<"$(
+  (
+    set -x
+    "$pip" list "$@" --not-required --outdated --format=json
+  ) | jq -r '.[] | "\(.name) \(.version) \(.latest_version)"'
+)"
+
+if [[ ${#outdatedVersions[@]} -eq 0 ]]; then
+  echo '   ... nothing needs updating'
+  exit 0
+fi
+
+for package in "${!outdatedVersions[@]}"; do
+  echo "   $package  ${outdatedVersions[$package]}  ->  ${latestVersions[$package]}"
+done
+read -rp "   OK? "
+[[ $REPLY =~ ^[Nn] ]] && exit 0
+[[ $REPLY =~ ^[Yy] ]] || exit 1
+
+set -x
+exec "$pip" install "$@" --upgrade -- "${!outdatedVersions[@]}"
--- home/bin/update-pip-user-packages.sh
+++ home/bin/update-pip-user-packages.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 #
 # Do an upgrade of all pip packages that were directly installed (i.e. not as a
-# dependency) in "user-site" (i.e. with `--user`; on typical Ubuntu installs,
-# these are libraries installed to `~/.local/lib/python3.x/site-packages/`,
-# command-line tools installed to `~/.local/bin`, and less commonly, ancillary
-# files placed into `~/.local/etc`).
+# dependency) via system `pip3`/`python3` in "user-site" (i.e. with `--user`).
+#
+# On typical Ubuntu installs, these are
+#
+# - libraries installed to `~/.local/lib/python3.x/site-packages/`,
+# - command-line tools installed to `~/.local/bin`, and,
+# - less commonly, ancillary files (e.g. completion) placed into `~/.local/etc`.
 
 set -euETo pipefail
 shopt -s inherit_errexit
@@ -20,16 +23,8 @@ readonly actualPip
 
 if ! [ "$actualPip" == "$expectedPip" ]; then
   echo "The current $pip tool is $actualPip rather than $expectedPip." >&2
-  echo "If inside of a virtualenv, deactivate it before running $0." >&2
+  echo "If using pyenv or inside virtualenv, deactivate before running $0." >&2
   exit 1
 fi
 
-mapfile -t packages \
-  < <("$pip" list --user --not-required --format=freeze | sed 's/==.*$//')
-
-if ! [ "${#packages[@]}" -gt 0 ]; then
-  echo "There does not seem to be any user packages installed via $pip." >&2
-  exit 1
-fi
-
-exec "$pip" install --user --upgrade -- "${packages[@]}"
+update-pip-packages --user
