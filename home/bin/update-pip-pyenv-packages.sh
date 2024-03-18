c503398599f59973935acab0a74694c1d8316ff0
Dave Shifflett <dave@corpulent.coffee>
2024-03-18 00:17:02 -0500

    update-pip-pyenv-packages fixes

--- home/bin/update-pip-pyenv-packages.sh
+++ home/bin/update-pip-pyenv-packages.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 #
-# Do an upgrade of all pip packages that were directly installed (i.e. not as a
-# dependency) inside of some `pyenv`-supplied installation.
+# Do an upgrade of all pip packages visible from `pyenv`-supplied installs. This
+# is usually `$PYENV_ROOT/versions/3.XX.X/lib/python3.XX/site-packages`, but
+# *can* be `--user`-installed things in `~/.local/lib/python3.XX/site-packages`,
+# notably for when the `python3.XX` version is not the "system" Python.
+#
+# This doesn't do anything to random project-specific `virtualenv` dependencies.
 
 set -euETo pipefail
 shopt -s inherit_errexit
 
-if [[ ! -v PYENV_ROOT || -z "$PYENV_ROOT" ]]; then
-  echo "$0 refusing to run w/o PYENV_ROOT" >&2
+if ! [[ -v PYENV_ROOT && -n $PYENV_ROOT && -x $PYENV_ROOT/bin/pyenv ]]; then
+  echo "Refusing to run $0 without valid PYENV_ROOT" >&2
   exit 1
 fi
 
-if ! command -v pyenv; then
-  [[ ":$PATH:" == *":$$PYENV_ROOT/bin:"* ]] || PATH="$PYENV_ROOT/bin:$PATH"
+if ! [[ ":$PATH:" == *":$PYENV_ROOT/bin:"* ]]; then
+  echo "Adding $PYENV_ROOT/bin to PATH for update" >&2
+  PATH="$PYENV_ROOT/bin:$PATH"
+fi
+if [[ $(type -t pyenv) == file ]]; then
+  echo 'Initializing pyenv shell integration for update' >&2
   eval "$(pyenv init -)"
 fi
 
@@ -22,15 +30,15 @@ while read -r installedVersion; do
     if [[ $installedVersion =~ ^3\.[0-9]+\.[0-9]+$ ]]; then
       installedVersions+=("$installedVersion")
     else
-      echo "unexpected pyenv install version $installedVersion" >&2
+      echo "Unexpected pyenv install version $installedVersion" >&2
       exit 1
     fi
   fi
 done <<<"$(pyenv versions --skip-aliases --skip-envs --bare)"
 
 if [[ ${#installedVersions[@]} -eq 0 ]]; then
-  echo 'no pyenv installs detected' >&2
-  exit 1
+  echo 'There are no pyenv-supplied installs detected right now' >&2
+  exit 0
 fi
 
 for version in "${installedVersions[@]}"; do
