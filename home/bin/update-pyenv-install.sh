77e7ae52b117b2e35052129f3782d7cfc4fe3e60
Dave Shifflett <dave@corpulent.coffee>
2024-03-28 21:56:53 -0500

    refactor: separated update-git-checkout

--- home/bin/update-pyenv-install.sh
+++ home/bin/update-pyenv-install.sh
@@ -1,39 +1,11 @@
 #!/usr/bin/env bash
-#
-# TODO: This is *mostly* like `update-nvm-install`; refactor out common bits.
 
 set -euETo pipefail
 shopt -s inherit_errexit
 
-if ! [[ -v PYENV_ROOT && -d "$PYENV_ROOT" && -d "$PYENV_ROOT/.git" ]]; then
-  echo 'pyenv does not seem to be installed here.' >&2
-  echo '(Expecting PYENV_ROOT to be set to a checked-out git repository.)' >&2
-  exit 1
-fi
-
-cd "$PYENV_ROOT"
-
-active=$(git describe --tags) # n.b. pyenv project uses LIGHT-WEIGHT tags
-if ! [[ "$active" == v*.*.* ]]; then
-  echo "Got '$active' for current HEAD of $PYENV_ROOT instead of version tag." >&2
-  exit 1
-fi
-
-git fetch --tags --prune --prune-tags
-latest=$(git tag --list 'v*.*.*' --sort version:refname | tail --lines=1)
-
-if [ -z "$latest" ]; then
-  echo 'Cannot determine the latest version.' >&2
-  exit 1
-elif [ "$active" == "$latest" ]; then
-  git status
-  echo
-  echo "$active is already the latest version of pyenv."
+if [[ -v PYENV_ROOT ]]; then
+  update-git-checkout "$PYENV_ROOT"
 else
-  echo
-  echo "Switching pyenv from $active to $latest..."
-  echo
-  git checkout "$latest"
-  echo
-  git status
+  echo 'pyenv does not seem to be installed here (expecting PYENV_ROOT)' >&2
+  exit 1
 fi
